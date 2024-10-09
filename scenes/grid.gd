dcedf2a197cb19eaf669d39d3270813053f6c4a1
Oradimi <loganargouse@gmail.com>
2024-10-10 02:37:00 +0800

    UI and functionality improved

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -1,19 +1,22 @@
-extends Node2D
+extends Control
 
-@onready var grid_container:= $GridContainer
+@onready var grid_container:= $HBox/Control/GridContainer
+@onready var preview_grid_container:= $HBox/Control/PreviewGridContainer
 @onready var grid_tiles: Array[ColorRect]
-@onready var item:= $Item
+@onready var preview_grid_tiles: Array[ColorRect]
+@onready var item:= $HBox/Item
 @onready var end:= $End
 @onready var progress_nodes: Array[Node] = [
-	$ProgressRed,
-	$ProgressBlue,
-	$ProgressGreen,
-	$ProgressYellow,
-	$ProgressPurple,
+	$HBox/Colors/ProgressRed,
+	$HBox/Colors/ProgressBlue,
+	$HBox/Colors/ProgressGreen,
+	$HBox/Colors/ProgressYellow,
+	$HBox/Colors/ProgressPurple,
 ]
 
 var currentShapeIndex: int
 var offset: Vector2i
+var max_offset: Vector2i
 var temp_grid_tiles: Array[Color]
 var progress: Dictionary
 
@@ -25,9 +28,17 @@ func _ready() -> void:
 	for child in children:
 		if child is ColorRect:
 			grid_tiles.append(child)
+	
+	var preview_children = preview_grid_container.get_children()
+	for child in preview_children:
+		if child is ColorRect:
+			grid_tiles.append(child)
+			preview_grid_tiles.append(child)
 
 	for i in len(grid_tiles):
 		temp_grid_tiles.append(grid_tiles[i].color)
+	
+	print(get_shape_size(item.data.shapes[currentShapeIndex]))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -49,14 +60,21 @@ func _process(delta: float) -> void:
 	if currentShapeIndex == item.data.shapes.size():
 		end.visible = true
 		return
+	
+	var shape_size = get_shape_size(item.data.shapes[currentShapeIndex])
+	var grid_size = Vector2i(5, 5)
 
-	offset.x = clampi(offset.x, 0, 2)
-	offset.y = clampi(offset.y, 0, 2)
+	max_offset = grid_size - shape_size
+	offset.x = clampi(offset.x, 0, max_offset.x)
+	offset.y = clampi(offset.y, 0, max_offset.y)
 
 	for i in range(0, 9):
+		var id = vector_get_grid_index(get_coords(i, 3) + offset, 5)
 		if item.data.shapes[currentShapeIndex].shape[i]:
-			grid_tiles[vector_get_grid_index(get_coords(
-				i, 3) + offset, 5)].color = item.data.shapes[currentShapeIndex].get_color()
+			preview_grid_tiles[id].color = item.data.shapes[currentShapeIndex].get_color()
+		else:
+			if id > -1 and id < 25:
+				preview_grid_tiles[id].color = Color(0, 0, 0, 0)
 
 	input_manager()
 
@@ -92,3 +110,22 @@ func get_children_of_type(obj, type):
 	for child in children:
 		if is_instance_of(obj, type):
 			grid_tiles.append(child)
+
+func get_shape_size(shapeData: ShapeData) -> Vector2i:
+	var min_x = 3
+	var min_y = 3
+	var max_x = 0
+	var max_y = 0
+	
+	for i in range(0, 9):
+		if shapeData.shape[i]:
+			var coords = get_coords(i, 3)
+			min_x = min(min_x, coords.x)
+			min_y = min(min_y, coords.y)
+			max_x = max(max_x, coords.x)
+			max_y = max(max_y, coords.y)
+	
+	var width = max_x - min_x + 1
+	var height = max_y - min_y + 1
+
+	return Vector2i(width, height)
--- scenes/item.gd
+++ scenes/item.gd
@@ -1,13 +1,13 @@
-extends Node2D
+extends Container
 class_name Item
 
 @export var data: ItemData
 
-@onready var item_name:= $Name
-@onready var item_description:= $Description
-@onready var item_icon:= $Icon
-@onready var item_shapes:= $Shapes
-@onready var item_traits:= $Traits
+@onready var item_name:= $MarginContainer/VBoxContainer/Name
+@onready var item_description:= $MarginContainer/VBoxContainer/Description
+@onready var item_icon:= $MarginContainer/VBoxContainer/Icon
+@onready var item_shapes:= $MarginContainer/VBoxContainer/Shapes
+@onready var item_traits:= $MarginContainer/VBoxContainer/Traits
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,7 +35,8 @@ func _process(delta: float) -> void:
 	pass
 
 func initialize_item() -> void:
-	var shape_count: int = randi_range(1, 6)
+	#var shape_count: int = randi_range(1, 6)
+	var shape_count: int = 8
 	print("Shape Count: " + str(shape_count))
 	for _n in range(0, shape_count):
 		data.shapes.append(ShapeData.new())
@@ -43,3 +44,20 @@ func initialize_item() -> void:
 		data.shapes[i].color = randi_range(0, ShapeData.ItemShapeColor.size() - 1)
 		for j in range(0, data.shapes[i].shape.size()):
 			data.shapes[i].shape[j] = bool(randi() & 1)
+		while !(data.shapes[i].shape[0] || data.shapes[i].shape[1] || data.shapes[i].shape[2]):
+			data.shapes[i].shape[0] = data.shapes[i].shape[3]
+			data.shapes[i].shape[1] = data.shapes[i].shape[4]
+			data.shapes[i].shape[2] = data.shapes[i].shape[5]
+			data.shapes[i].shape[3] = data.shapes[i].shape[6]
+			data.shapes[i].shape[4] = data.shapes[i].shape[7]
+			data.shapes[i].shape[5] = data.shapes[i].shape[8]
+			data.shapes[i].shape[6] = false
+			data.shapes[i].shape[7] = false
+			data.shapes[i].shape[8] = false
+		while !(data.shapes[i].shape[0] || data.shapes[i].shape[3]):
+			data.shapes[i].shape[0] = data.shapes[i].shape[1]
+			data.shapes[i].shape[3] = data.shapes[i].shape[4]
+			data.shapes[i].shape[1] = data.shapes[i].shape[2]
+			data.shapes[i].shape[4] = data.shapes[i].shape[5]
+			data.shapes[i].shape[2] = false
+			data.shapes[i].shape[5] = false
